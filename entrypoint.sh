409ee0a3db57ab1254be6289cd300a231152c458
Adrian Dvergsdal <adrian.dvergsdal@competit.no>
2015-10-22 15:16:11 +0200

    Give munin ownership of data dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,8 @@ sed -i "s/127.0.0.1/$hostIp/g" /etc/munin/munin.conf
 interval=${MUNIN_INTERVAL:-5}
 echo "*/$MUNIN_INTERVAL * * * * munin /usr/bin/munin-cron" >> /etc/crontab
 
+# Give munin ownership of data dir
+chown -R munin /var/lib/munin
+
 # Boot it up!
 exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
